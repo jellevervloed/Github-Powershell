fdaa83ae69fe727b948fa6106788b76bf898ef5b
jellevervloed <48052849+jellevervloed@users.noreply.github.com>
2019-03-27 11:34:47 +0100

    Add files via upload

---
+++ Mailverturen.ps1
@@ -0,0 +1,9 @@
+﻿$From = "jellevervloed@hotmail.nl"
+$To = "jellevervloed@hotmail.nl"
+$Cc = "jellevervloed@hotmail.nl"
+$Attachment = "D:\Opdracht7.docx"
+$Subject = "een mooi mailtje van powershell"
+$Body = "een hele goede middag"
+$SMTPServer = "smtp.office365.com"
+$SMTPPort = "587"
+Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential (Get-Credential) -Attachments $Attachment -DeliveryNotificationOption OnSuccess
